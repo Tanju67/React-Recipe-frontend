bdbd1ccee0a5b5819721d17f3fbb1cf3de63f0dd
Tanju Özer <tanjuozer64@gmail.com>
2024-03-01 12:03:09 +0100

    make a setup of index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={null} />
+        <Route index element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
